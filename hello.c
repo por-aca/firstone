4883b415e61fdc685eae9c7352f093b457ee8e5c
vyanezgx <victor.yanez230689@gmail.com>
2017-06-17 15:21:20 +0000

    secondcomment

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main()
 {
 	char var[3];
 	
-	printf("cuales son tus iniciales:" );
+	printf("cuales son tus iniciales y nombre:" );
 	scanf("%s",&var);
 	printf("%s", var);
 	system ("Pause>nul");

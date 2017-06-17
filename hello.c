4d94a73dce8c5649a5e1a2823f6809e8cb6eea37
vyanezgx <victor.yanez230689@gmail.com>
2017-06-17 15:07:46 +0000

    myfirstcommit

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char var[3];
+	
+	printf("cuales son tus iniciales:" );
+	scanf("%s",&var);
+	printf("%s", var);
+	system ("Pause>nul");
+	return 0;
+}
+
+
